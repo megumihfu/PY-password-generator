981b2c9943e9f3b9ae24376a258144cd3b956b2f
megumihfu <idaffef@gmail.com>
2023-05-22 22:22:39 +0300

    correction des erreurs, retourne bien le bon mdp et pas un des premiers générés

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -5,12 +5,6 @@ class PasswordGenerator:
     def __init__(self, size, password):
         self.size = size
         self.password = password
-
-    def password_size(self):
-        self.size = int(input("Enter the size needed for your new password (highter than 8): "))
-        if self.size < 8:
-            self.size = int(input("Please retry with a size highter than 8"))
-        return self.size
     
     def password_generator(self):
         password = string.ascii_uppercase + string.ascii_lowercase + string.digits
@@ -19,35 +13,32 @@ class PasswordGenerator:
         return ''.join(self.password)
     
     def check_password_security(self):
-        #print("dans check_security : ", ''.join(self.password))
         has_digit = False
         has_upper = False
         has_lower = False
 
-        for x in self.password:
+        for x in self.password: #mettre dans un tant que pour que des que les conditions sont remplies -> next step
             if x.isdigit():
                 has_digit = True
             elif x.islower():
                 has_lower = True
             elif x.isupper():
                 has_upper = True
-            
+
         if has_digit & has_lower & has_upper:
-            #print("The password generated is strong enough")
+            print("The password generated is strong enough")
             return True
+        
         else:
             print("The password generated isn't strong enough")
             return False
     
     def final_password(self):
-        password = self.check_password_security()
-        if password == True:
+        if self.check_password_security():# == True:
             #print("pas besoin de regenérer un nouveau mdp")
             return ''.join(self.password)
         else:
-            #print("The password generated isn't strong enough for you")
-            self.password_generator()
-            self.check_password_security()
-            self.final_password()
+            self.password = self.password_generator()
+            return self.final_password() #pourquoi qd condition ok et que password = true alors on revient ici ??
 
     
--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from PasswordGenerator import PasswordGenerator
 if __name__ == "__main__":
     size = int(input("Enter the size needed for your new password : "))
     if size < 8:
-        size = int(input("Please retry with a size highter than 8"))
+        while size < 8:
+            size = int(input("Please retry with a size highter than 8 : "))
     
     password = ""
     generator = PasswordGenerator(size, password) #creation de l'instance
     
-    generator.password_generator()
-    final_password = generator.final_password()
-    print("Password generated : ", final_password)
+    generator.password_generator() #tester de mettre final_password = ici plutot
+    password = generator.final_password()
+    print("Password generated : ", password)
